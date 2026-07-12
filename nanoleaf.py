2c18c458f8f852ae360be528bd49a845913f4aa7
aurora-ftc <harry.wiencek@gmail.com>
2026-07-11 18:13:54 -0700

    First Commit

---
+++ nanoleaf.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+class Nanoleaf:
+    def __init__():
+        load_dotenv()
+        os.getenv("NANOLEAF_IP")
+        
+        pass
+    def on(): 
+        pass
+    def off():
+        pass
---
+++ xbox.py
@@ -0,0 +1,5 @@
+class Xbox:
+    def __init__():
+        pass
+    def is_on() -> bool:
+        pass
